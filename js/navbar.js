cd17b26fdd85d5eb1510fb18fb585e266af6f944
@marichu_kt <marichu.private@gmail.com>
2025-07-17 21:31:10 +0200

    Delete js directory

--- js/navbar.js
+++
@@ -1,5 +0,0 @@
-/* █▄░█ ▄▀█ █░█ █▄▄ ▄▀█ █▀█ ░ ░░█ █▀ */
-/* █░▀█ █▀█ ▀▄▀ █▄█ █▀█ █▀▄ ▄ █▄█ ▄█ */
-
-// Alterna la clase "open" en el body para abrir o cerrar el menú
-const toggleMenuOpen = () => document.body.classList.toggle("open");
